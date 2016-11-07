61e48558e15204eb2a8346920c7077d2e1c9c0b5
jvlara <jvlara>
2016-11-07 12:02:24 +0100

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/my-app.html',
+  styleUrls: ['app/my-app.css']
+})
+export class AppComponent { }
